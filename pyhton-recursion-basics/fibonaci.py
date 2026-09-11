2392c96c24f0046ef929cf445956b710f8035ce5
NinjaOfNeurons <dev.karanpreet@gmail.com>
2026-09-11 13:40:59 +0530

    Fix Fibonacci function to handle base cases correctly and update print statement for testing

--- pyhton-recursion-basics/fibonaci.py
+++ pyhton-recursion-basics/fibonaci.py
@@ -1,12 +1,14 @@
 def fibo(n,n1,n2,count):
     if n == 0:
         print(0)
+        return
     elif n==1:
         print(0,1)
+        return 
     if count <= n:
         nth = n1+n2
         print(n1)
         fibo(n, n1=n2,n2=nth,count= count+1)
 
 
-print(fibo(25,0,1,0))
+print(fibo(1,0,1,0))
